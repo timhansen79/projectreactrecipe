751a55929d53a0ae3e5aba4073311f249535f46e
timh <tim@tim.hansen.se>
2020-12-20 21:24:09 +0100

    async done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Recipe from "./recipe";
 
 function App() {
   const APP_ID = "c83aaa42";
   const APP_KEY = "35f38215934da4bd7efba0c1026d8929	";
 
-  /* const exampleRequest = ``; */
-
-  /* const [counter, setCounter] = useState(0); */
+  //Skapa state (här hamnar alal våra recept)
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   //tar en arrow function som parameter
   useEffect(() => {
     getRecipes();
     /* console.log("Effect has been run"); */
     //om vi bara vill att effect ska köras en gång lägg till tom array dvs , []
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
-    console.log(data);
+    /* console.log(data.hits); */
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = (event) => {
+    setSearch(event.target.value);
+    console.log(search);
+  };
+
+  const getSearch = (event) => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch("");
   };
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
-      {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1> */}
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/recipe.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredients) => (
+          <li>{ingredients.text}</li>
+        ))}
+      </ol>
+      <img src={image} alt="" />
+    </div>
+  );
+};
+
+export default Recipe;
